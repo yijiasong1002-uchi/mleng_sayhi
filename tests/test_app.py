7cbf35005c902bb474980d4566243ef1fc2a5a57
yijiasong1002-uchi <yijiasong@uchicago.edu>
2025-08-05 09:36:18 +0800

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
 # Run the tests with: pytest tests/test_app.py
-from ..app import add_numbers
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import add_numbers
 
 def test_add_numbers():
-    assert 3 == add_numbers(1,2)
+    assert 3 == add_numbers(1, 2)
